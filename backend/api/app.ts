3003aa69c09c04d4a978640ba6663dff537b35dd
Nebojsa Lukic <nlukic129@gmail.com>
2024-03-15 00:14:55 +0100

    Add Docker configuration for MongoDB

--- backend/api/app.ts
+++ backend/api/app.ts
@@ -1,12 +1,26 @@
 import express, { Request, Response } from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+const PORT = process.env.PORT;
 
 const app = express();
-const port = 3000;
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello, World!");
 });
 
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
-});
+const mongodbUri = process.env.MONGO_URI || "";
+
+mongoose
+  .connect(mongodbUri, {
+    dbName: process.env.DB_NAME,
+  })
+  .then(() => {
+    console.log("Mongodb connected....");
+    app.listen(PORT, () => {
+      console.log(`Server running at http://localhost:${PORT}`);
+    });
+  })
+  .catch((err) => console.log(err.message));
